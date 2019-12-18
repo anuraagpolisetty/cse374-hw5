cb04d976d419c3fb5cc656fbb4c201370c9fac24
anuraagpolisetty <anuraag@uw.edu>
2019-12-17 23:59:04 -0800

    Updated with the correct files that pass autograder

--- dict_main.c
+++ dict_main.c
@@ -4,23 +4,24 @@
 
 #include "dict.h"
 
-int main(int argc, char *argv[argc+1]) {
+int main(int argc, char *argv[argc + 1]) {
   struct dict_t *dict = dictionary_new(argv[2], 5e5);
-  printf("new dict made!");
+  printf("new dict made");
   if (!strcmp(argv[1], "gen")) {
     if (dictionary_generate(dict, argv[3]) < 0) {
       printf("Error generating dictionary\n");
       return EXIT_FAILURE;
     }
   } else if (!strcmp(argv[1], "load")) {
-    if(!dictionary_load(dict)) {
+    if (!dictionary_load(dict)) {
       printf("Error loading dictionary\n");
       return EXIT_FAILURE;
     }
   }
+  printf("new dictionary loaded");
   char *addr = dictionary_exists(dict, "cat");
   if (addr) {
-    printf("i haz cat, it is at: %p\n", (void*)addr);
+    printf("i haz cat, it is at: %p\n", (void *)addr);
   }
   dictionary_close(dict);
   return EXIT_SUCCESS;
